575114daf47cba6c375ce6a845536a105b22c574
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-06-29 10:47:44 -0600

    404 page created, images added, style added.

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function NoMatch() {
     return (
-        <div>
-            <h1>404 Not Found</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6 offset-m3">
+                    <div class="card butlr-blue">
+                        <div class="card-content white-text">
+                            <span class="card-title font3">Oops...</span>
+                            <h3 class="font2">404 Not Found <span role="img" aria-label="Face with rolling eyes"> 🙄</span></h3>
+                        </div>
+                        <div class="card-action">
+                            <a href="/" class="waves-effect waves-light btn butlr-green font2"><i class="material-icons left">home</i>Back
+            To Home</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
