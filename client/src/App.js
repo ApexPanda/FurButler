efc259c29e44dd093359b7693b9cfc140ce947b6
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-06-29 10:21:34 -0600

    Added react page routes with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Results from "./pages/Results";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import NoMatch from "./pages/NoMatch";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          {/* <Nav /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/results/:role" component={Results} />
+            <Route exact path="/profile/:id" component={Profile} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
