20aed7b31c337b5a38ed6eac69983f7a51996a1b
emssslay <emily.robinson@iechs.org>
2019-06-29 11:16:00 -0600

    new server and routes

--- Models/index.js
+++ Models/index.js
@@ -2,7 +2,7 @@
 
 var fs = require("fs");
 var path = require("path");
-var Sequelize = require("sequelize");
+var Sequelize = require("./node_modules/sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
