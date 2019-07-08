86f18d0d1b615f61484ce0af3949c48e6966c04f
emssslay <emily.robinson@iechs.org>
2019-07-08 09:36:36 -0600

    reverting again

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+// eslint-disable-next-line no-unused-vars
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Password",
+    database: "	g57v06z3jkgspbrm"
+  });
+}
