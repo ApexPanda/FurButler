9c37c2daaffa5bb61f8992f2fbc5a5fe52052ec2
emssslay <emily.robinson@iechs.org>
2019-07-08 17:48:50 -0600

    fixed models references

--- routes/userLoginRoutes.js
+++ routes/userLoginRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
+const db = require("../Models");
 
 // Bcrypt==========================
 const bcrypt = require("bcrypt");
