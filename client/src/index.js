dcf38c07fe6ad24fe93707aa2e3a6d8fcf87c8ce
Kayla Tryon <dawnka22@gmail.com>
2019-07-10 09:50:27 -0600

    src/components updated with react chat widget

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 // React-Chat-Widget vvv
 import PropTypes from "prop-types";
 import { Provider } from "react-redux";
@@ -7,6 +8,7 @@ import { Provider } from "react-redux";
 import Widget from "./components/Widget";
 import store from "../src/store/store";
 // React-Chat-Widget ^^^
+
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
