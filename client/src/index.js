380a32c275d44eb2f2cb3688bdf3e1305c87b3a5
emssslay <emily.robinson@iechs.org>
2019-07-10 20:57:19 -0600

    conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// React-Chat-Widget vvv
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+
+import Widget from "./components/Widget";
+import store from "../src/store/store";
+// React-Chat-Widget ^^^
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
