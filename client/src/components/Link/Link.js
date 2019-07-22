c7f0495727aa14b713134b89c8718f8e7289ef56
emssslay <emily.robinson@iechs.org>
2019-07-22 10:32:04 -0600

    not working

---
+++ client/src/components/Link/Link.js
@@ -0,0 +1,22 @@
+// import React, { Component } from "react";
+// import API from "../../utils/API";
+
+// class Link extends Component {
+
+//     state = {
+//         profileLink: "/profile/" + this.props.id,
+//         rating: null,
+//     };
+
+
+
+
+//     render() {
+
+//     return (
+//         <a href={this.state.link} className="butlr-yellow-text font2"></a>
+//         )
+//     }
+// }
+
+// export default Link;
